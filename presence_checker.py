d2a2aaee80bc5842cebb335a38d3439df54d2e1d
Nick Jensen <nickthejensen@gmail.com>
2017-07-06 10:53:57 -0600

    When subscriber dies, presence checker requests its ping. Subscriber listens for that request on its own channel: /GUID

--- presence_checker.py
+++ presence_checker.py
@@ -14,7 +14,12 @@ consider that client dead, and
 the is_dead flag in the database is set.
 '''
 
-def check_presence(posts, lcycle) :
+def request_ping(mqtt_client, GUID) :
+    dump = json.dumps({'request_ping':True})
+    print('Sending message!')
+    mqtt_client.publish("/%s" %GUID, payload=dump)
+
+def check_presence(posts, mqtt_client, lcycle) :
     start = int(time.time())
     # Find all living clients in the database
     for entry in posts.find({'is_dead':False}) :
@@ -23,6 +28,7 @@ def check_presence(posts, lcycle) :
         if (int(time.time()) - seen) > 2 * lcycle :
             # Update database with notice of death
             result = posts.update_one({'client':entry.get('client')}, {'$set':{'is_dead':True}}, upsert=False)
+            request_ping(mqtt_client, entry.get('client'))
             print('%s died.' %entry.get('client'))
     next_check = start + lcycle
     difference = next_check - int(time.time())
@@ -33,14 +39,18 @@ def main(config) :
     dbclient = MongoClient('mongodb://%s:%d' %(config['mongo_host'], config['mongo_port']))
     db = dbclient.test_database
     posts = db.posts
+    mqtt_client = paho.Client()
+    mqtt_client.connect(config['mqtt_host'], config['mqtt_port'], 60)
     while(True) :
-        check_presence(posts, config['lcycle'])
+        check_presence(posts, mqtt_client, config['lcycle'])
 
 if __name__ == '__main__' :
+    import paho.mqtt.client as paho
     from pymongo import MongoClient
     from read_config import read_config
     import time
     import random
+    import json
     random.seed()
     main(read_config())
 
--- subscriber.py
+++ subscriber.py
@@ -10,26 +10,32 @@ import json
 from read_config import read_config
 random.seed()
 
-def on_message(mosq, obj, msg) :
-    print('Received message:')
-    print(json.loads(msg.payload))
+GUID = None
+
+def on_message(client, userdata, msg) :
+    content = json.loads(msg.payload)
+    if content['request_ping'] :
+        global GUID
+        dump = json.dumps({'client':GUID})
+        client.publish("/sendping", payload=dump)
+        
 
 if __name__ == '__main__':
     config = read_config()
     client = paho.Client()
+    client.on_message = on_message
 
     #client.tls_set('root.ca', certfile='c1.crt', keyfile='c1.key')
     client.connect(config['mqtt_host'], config['mqtt_port'], 60)
 
-    #GUID = 'GUID-%d' %random.randint(1000,9999)
-    GUID = 'GUID-1822'
+    GUID = 'GUID-%d' %random.randint(1000,9999)
+    #GUID = 'GUID-1822'
     client.subscribe("/%s" %GUID, 0)
-    client.on_message = on_message
 
     print('ID: %s' %GUID)
     dump = json.dumps({'client':GUID})
     while client.loop() == 0:
         client.publish("/sendping", payload=dump)
-        time.sleep(3)
+        time.sleep(config['lcycle'])
         pass
 
